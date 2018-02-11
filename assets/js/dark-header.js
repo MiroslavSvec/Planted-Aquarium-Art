a798739d696aa70fc39d6103b6223d700c38f7cd
Miroslav Svec <vazkez.svec@gmail.com>
2018-02-11 08:11:43 +0000

    Replacing / Removing code which is not my

--- assets/js/dark-header.js
+++ assets/js/dark-header.js
@@ -1,9 +1,9 @@
-$(window).scroll(function() {
-	var scroll = $(window).scrollTop();
-
-	if (scroll >= 300) {
-		$(".transparent-header").addClass("dark-header");
-	} else {
-		$(".transparent-header").removeClass("dark-header");
-	}
+$(document).ready(function() {
+	$(document).scroll(function() {
+		if ($(document).scrollTop() > 400) {
+			$(".transparent-header").addClass("dark-header");
+		} else {
+			$(".transparent-header").removeClass("dark-header");
+		}
+	});
 });
