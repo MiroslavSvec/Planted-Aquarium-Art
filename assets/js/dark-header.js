fd039a2e82ff5fa7738002b36ea62c802684463c
Miroslav Svec <vazkez.svec@gmail.com>
2018-02-13 00:58:36 +0000

    Cleaning up the code / Removing code which is not my

--- assets/js/dark-header.js
+++ assets/js/dark-header.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 	$(document).scroll(function() {
-		if ($(document).scrollTop() > 400) {
-			$(".transparent-header").addClass("dark-header");
+		if ($(document).scrollTop() >= 5) {
+			$("#header").addClass("dark-header");
 		} else {
-			$(".transparent-header").removeClass("dark-header");
+			$("#header").removeClass("dark-header");
 		}
 	});
 });
