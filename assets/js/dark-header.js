af1289d41ef65e4735bf48aa2e0b451a862b25ff
Miroslav Svec <vazkez.svec@gmail.com>
2018-02-13 17:20:51 +0000

    Removing code which is not my / makaing the code simpler also better naming

--- assets/js/dark-header.js
+++ assets/js/dark-header.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$(document).scroll(function() {
-		if ($(document).scrollTop() >= 5) {
+		if ($(document).scrollTop() >= 650) {
 			$("#header").addClass("dark-header");
 		} else {
 			$("#header").removeClass("dark-header");
