06230020a375503a1dabb75500d12a32bba0586b
MiroslavSvec <33287498+MiroslavSvec@users.noreply.github.com>
2018-03-17 23:35:25 +0000

    Fixed nav bar navigation issues improved user xp

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,7 @@
-/* Navbar */
+/* Nav */
 
 $(document).ready(function() {
-	$("#main-links li:first").insertAfter($("#main-links li:nth-child(4)"));
-	$("#sub-links li:first").insertAfter($("#sub-links li:nth-child(4)"));
-	$(".visible").addClass("section-nav");
-	$(".visible").removeClass("visible");
-	$("#sub-links ol:first").addClass("visible");
+	$("#header").hover(function() {
+		$("#sub-links>li>ol").slideToggle("section-nav");
+	});
 });
