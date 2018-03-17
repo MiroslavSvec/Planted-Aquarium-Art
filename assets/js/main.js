01ff5b0defb329eacfcfc8e903d5a4a7add1a7a6
MiroslavSvec <33287498+MiroslavSvec@users.noreply.github.com>
2018-03-17 04:46:51 +0000

    Redesing nav bar to better suits user

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,9 @@
 /* Navbar */
 
 $(document).ready(function() {
-	$("#home").hover(function() {
-		$("#home>ol").slideToggle("visible");
-	});		
-	$("#gallery").hover(function() {
-		$("#gallery>ol").slideToggle("visible");
-	});	
-	$("#about-us").hover(function() {
-		$("#about-us>ol").slideToggle("visible");
-	});
-	$("#your-project").hover(function() {
-		$("#your-project>ol").slideToggle("visible");
-	});	
-	$("#profile").hover(function() {
-		$("#profile>ol").slideToggle("visible");
-	});
-	$("#shopping-cart").hover(function() {
-		$("#shopping-cart>ol").slideToggle("visible");
-	});
+	$("#main-links li:first").insertAfter($("#main-links li:nth-child(4)"));
+	$("#sub-links li:first").insertAfter($("#sub-links li:nth-child(4)"));
+	$(".visible").addClass("section-nav");
+	$(".visible").removeClass("visible");
+	$("#sub-links ol:first").addClass("visible");
 });
