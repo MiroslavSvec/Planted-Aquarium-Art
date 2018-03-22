bd8d7814efd6f6f8fbac105cbef5a8c4b288c61c
Miroslav Svec <vazkez.svec@gmail.com>
2018-03-22 04:05:21 +0000

    Added box-shadow to gallery cards / improved footer links animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,26 @@
 /* Nav */
 
 $(document).ready(function() {
-	$("#header").hover(function() {
-		$("#sub-links>li>ol").slideToggle("section-nav");
-	});
+	if ($(window).width() <= 968) {
+		$("#header").click(function() {
+			$("#sub-links>li>ol").slideToggle("section-nav");
+		});
+	} else {
+		$("#header").hover(function() {
+			$("#sub-links>li>ol").slideToggle("section-nav");
+		});
+	}
+});
+
+/* Footer Social Links */
+
+$(document).ready(function() {
+	if ($(window).width() >= 1400) {
+		$(".footer-links .fab").hover(function() {
+			$(".fa-twitter").toggleClass("twitter");
+			$(".fa-facebook-f").toggleClass("facebook");
+			$(".fa-linkedin-in").toggleClass("linkedin");
+			$(".fa-instagram").toggleClass("instagram");
+		});
+	}
 });
