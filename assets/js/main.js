21fd4a42c6e3e2e0c8986e4c04e489cbbbd724c3
Miroslav Svec <vazkez.svec@gmail.com>
2018-04-10 03:51:30 +0100

    Added more content / script for form validation

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,59 @@ $(document).ready(function() {
 	});
 });
 
+/* Alert popup */
+
+function alerts_box_animation() {
+	$("#message-box").html(warningMessage);
+	$("#alerts").slideDown(500);
+	var y = setTimeout(slide, 2000);
+	function slide() {
+		$("#alerts").slideUp(1000);
+	}
+}
+
+/* Form Validation */
+
+
+$(document).ready(function() {
+	$("#name, #userEmail, #message").click(function() {
+		$(this).css("border", "1px solid transparent");
+	});
+});
+
+function validForm() {
+	var name = [$("#name").val(), "#name"];
+	var email = [$("#userEmail").val(), "#userEmail"];
+	var message = [$("#message").val(), "#message"];
+
+	var formColumn = [name, email, message];
+
+	for (var i = 0; i < formColumn.length; i++) {
+		console.log(formColumn[i][1]); /* For testing */
+
+	
+
+		if (formColumn[i][0] == null || formColumn[i][0] == "") {
+			if (message.length <= 15) {
+				warningMessage =
+					"PLease fill up the form... <br> Your message needs to be longer then 15 characters.";
+			}
+			if (formColumn[i][0] == "") {
+				$(formColumn[i][1]).css("border", "1px solid red");
+			} else {
+				warningMessage = "PLease fill up the form...";
+			}
+			alerts_box_animation(warningMessage);
+			return false;
+		}
+	}
+	warningMessage = "Your message has been sent. <br> Thank you!";
+	alerts_box_animation(warningMessage);
+	$("#contactform").trigger("reset");
+
+	return false; /* For testing */
+}
+
 /* Footer Social Links */
 
 $(document).ready(function() {
@@ -40,11 +93,10 @@ $(document).ready(function() {
 /* Slide Show */
 
 $(document).ready(function() {
-	
 	var func = setInterval(slideShow, 0.0001);
 
 	function slideShow() {
-		$(".quote-container .quote:gt(0)").hide(); 
+		$(".quote-container .quote:gt(0)").hide();
 		$(".quote-container>:first-child")
 			.fadeOut()
 			.next(".quote")
@@ -60,7 +112,7 @@ $(document).ready(function() {
 		func = setInterval(slideShow, 0.0001);
 		slideShow();
 	});
-	
+
 	$("#previous-button").click(function() {
 		clearInterval(func);
 		func = setInterval(slideShow, 0.0001);
